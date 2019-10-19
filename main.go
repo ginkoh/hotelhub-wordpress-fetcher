f8f6df34cf6b65042e38cfe0e86b75f4c9a013b4
Gian <gianszabernardo@gmail.com>
2019-10-19 20:12:22 -0300

    Create a utils package

--- main.go
+++ main.go
@@ -5,57 +5,47 @@ import (
 	"github.com/gorilla/mux"
 	//"fmt"
 	typedefs "./typedefs"
+	utils "./utils"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
-
-func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-}
-
-func setContentType(w *http.ResponseWriter) {
-	(*w).Header().Set("Content-Type", "application/json")
-}
-
 func ServePostList(w http.ResponseWriter, r *http.Request) {
-	setContentType(&w)
-	enableCors(&w)
+	// Set the content type to JSON.
+	utils.SetContentType(&w)
 
+	// Enable CORS on the server.
+	utils.EnableCors(&w)
+
+	// URL to fetch the content from.
 	url := "https://www.hotelflow.com.br/wp-json/wp/v2/posts"
 
-	// Creates a client connection.
+	// Makes a GET request to the provided URL.
 	response, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
+	utils.LogError(err)
+
 	// Read the response.
 	responseJSON, readErr := ioutil.ReadAll(response.Body)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
+	utils.LogError(readErr)
 
+	// Initialize the struct variable to pass the JSON response as the content by reference.
 	var postsList []typedefs.Posts
 
-	// Parse the response.
+	// Parse the JSON response.
 	jsonErr := json.Unmarshal(responseJSON, &postsList)
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
+	utils.LogError(jsonErr)
 
+	// Encode the JSON.
 	json.NewEncoder(w).Encode(postsList)
 }
 
 func main() {
-
+	// Create the main mux router.
 	router := mux.NewRouter()
 
+	// Create the routes.
 	router.HandleFunc("/posts", ServePostList).Methods("GET")
 
-	// Print it to the screen.
+	// Serve the routes.
 	http.ListenAndServe(":5000", router)
-
-
-
 }
---
+++ utils/http-utils.go
@@ -0,0 +1,11 @@
+package utils
+
+import "net/http"
+
+func EnableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func SetContentType(w *http.ResponseWriter) {
+	(*w).Header().Set("Content-Type", "application/json")
+}
